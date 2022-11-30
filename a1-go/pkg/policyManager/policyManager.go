ecc0d18801c4dc93a5e5ba5ef6d676670ed03074
naman.gupta <naman.gupta@samsung.com>
2022-11-30 23:03:35 +0530

    RMR handler for A1 policy query
    RMR handler for A1 policy query(messagetype 20012) to xapp
    
    Signed-off-by: naman.gupta <naman.gupta@samsung.com>
    Change-Id: I7b2a031cf19d8a140f294c4c42647a4582ba0133

--- a1-go/pkg/policyManager/policyManager.go
+++ a1-go/pkg/policyManager/policyManager.go
@@ -27,9 +27,14 @@ import (
 	"strconv"
 )
 
+var policyTypeNotFoundError = errors.New("Policy Type Not Found")
+var policyInstanceNotFoundError = errors.New("Policy Instance Not Found")
+
 const (
-	a1HandlerPrefix = "a1.policy_handler."
-	a1MediatorNs    = "A1m_ns"
+	a1HandlerPrefix  = "a1.policy_handler."
+	a1PolicyPrefix   = "a1.policy_type."
+	a1MediatorNs     = "A1m_ns"
+	a1InstancePrefix = "a1.policy_instance."
 )
 
 func NewPolicyManager(sdl *sdlgo.SyncStorage) *PolicyManager {
@@ -51,4 +56,79 @@ func (pm *PolicyManager) SetPolicyInstanceStatus(policyTypeId int, policyInstanc
 		return err
 	}
 	return nil
+}
+
+func (im *PolicyManager) GetAllPolicyInstance(policyTypeId int) ([]models.PolicyInstanceID, error) {
+	a1.Logger.Debug("GetAllPolicyInstance")
+	var policyTypeInstances = []models.PolicyInstanceID{}
+
+	keys, err := im.db.GetAll("A1m_ns")
+
+	if err != nil {
+		a1.Logger.Error("error in retrieving policy. err: %v", err)
+		return policyTypeInstances, err
+	}
+	a1.Logger.Debug("keys : %+v", keys)
+	typekey := a1InstancePrefix + strconv.FormatInt((int64(policyTypeId)), 10) + "."
+
+	for _, key := range keys {
+		if strings.HasPrefix(strings.TrimLeft(key, " "), typekey) {
+			pti := strings.Split(strings.Trim(key, " "), typekey)[1]
+			a1.Logger.Debug("pti %+v", pti)
+			policyTypeInstances = append(policyTypeInstances, models.PolicyInstanceID(pti))
+		}
+	}
+
+	if len(policyTypeInstances) == 0 {
+		a1.Logger.Debug("policy instance Not Present  ")
+	}
+
+	a1.Logger.Debug("return : %+v", policyTypeInstances)
+	return policyTypeInstances, nil
+}
+
+func (im *PolicyManager) GetPolicyInstance(policyTypeId models.PolicyTypeID, policyInstanceID models.PolicyInstanceID) (interface{}, error) {
+	a1.Logger.Debug("GetPolicyInstance1")
+
+	var keys [1]string
+
+	typekey := a1PolicyPrefix + strconv.FormatInt((int64(policyTypeId)), 10)
+	keys[0] = typekey
+
+	a1.Logger.Debug("key1 : %+v", typekey)
+
+	valmap, err := im.db.Get(a1MediatorNs, keys[:])
+	if len(valmap) == 0 {
+		a1.Logger.Debug("policy type Not Present for policyid : %v", policyTypeId)
+		return "{}", policyTypeNotFoundError
+	}
+
+	if err != nil {
+		a1.Logger.Error("error in retrieving policy type. err: %v", err)
+		return "{}", err
+	}
+
+	if valmap[typekey] == nil {
+		a1.Logger.Debug("policy type Not Present for policyid : %v", policyTypeId)
+		return "{}", policyTypeNotFoundError
+	}
+
+	a1.Logger.Debug("keysmap : %+v", valmap[typekey])
+
+	instancekey := a1InstancePrefix + strconv.FormatInt((int64(policyTypeId)), 10) + "." + string(policyInstanceID)
+	a1.Logger.Debug("key2 : %+v", instancekey)
+	keys[0] = instancekey
+	instanceMap, err := im.db.Get(a1MediatorNs, keys[:])
+	if err != nil {
+		a1.Logger.Error("policy instance error : %v", err)
+	}
+	a1.Logger.Debug("policyinstancetype map : %+v", instanceMap)
+
+	if instanceMap[instancekey] == nil {
+		a1.Logger.Debug("policy instance Not Present for policyinstaneid : %v", policyInstanceID)
+		return "{}", policyInstanceNotFoundError
+	}
+
+	valStr := fmt.Sprint(instanceMap[instancekey])
+	return valStr, nil
 }
